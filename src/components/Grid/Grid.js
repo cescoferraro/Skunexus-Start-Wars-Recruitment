d7be5489cc5409f6b538580dcbb207fb72183c29
cescoferraro <francescoaferraro@gmail.com>
2022-03-08 01:40:02 -0300

    chore: make Grid component conditionally show actions

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,28 +1,48 @@
 import './Grid.css';
+import {Button, Table} from "reactstrap";
 
 function Grid({data: {header = [], values = [], actions = []}}) {
-  return (
-    <table className='gridTable'>
-      <thead>
-        <tr>
-          {header.map(colName => <th key={`${colName}-header`}>{colName}</th>)}
-          {!!actions.length && <th>Actions</th>}
-        </tr>
-      </thead>
-      <tbody>
-        {values.map((row, index) => (
-          <tr key={row.url} style={{height: 100}}>
-            {header.map((colName) => <td key={colName}>{row[colName]}</td>)}
-            {!!actions.length &&
-              <td className='gridActions'>
-                {actions.map(({label, action}) => <button key={`${label}-${index}`} onClick={() => action(row)}>{label}</button>)}
-              </td>
-            }
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+    const hasActions = !!actions.length;
+    return (
+        <Table striped={true} className='gridTable'>
+            <thead>
+            <tr>
+                {header.map(colName => <th key={`${colName}-header`}>{colName}</th>)}
+                {hasActions && <th>Actions</th>}
+            </tr>
+            </thead>
+            <tbody>
+            {values.map((row, index) => {
+                return (
+                    <tr key={row.url} style={{height: 100}}>
+                        {header.map((colName) => <td key={colName}>{row[colName]}</td>)}
+                        {hasActions &&
+                            <td className='gridActions'>
+                                {actions.map(({label, action, show}) => {
+                                        if (show) {
+                                            if (!show(row)) {
+                                                return
+                                            }
+                                        }
+                                        return <Button
+                                            color={"primary"}
+                                            key={`${label}-${index}`}
+                                            onClick={() => action(row)}
+
+                                            icon="navbar-toggler-icon"
+                                        >
+                                            {label}
+                                        </Button>;
+                                    }
+                                )}
+                            </td>
+                        }
+                    </tr>
+                );
+            })}
+            </tbody>
+        </Table>
+    );
 }
 
 export default Grid;
